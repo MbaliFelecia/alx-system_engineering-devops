36bebd513308da82813eaa8e2a156249b8b85c91
MbaliFelecia <ramakgashambali@gmail.com>
2023-11-28 05:56:06 -0800

    puppet for task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,3 +3,4 @@
 exec {'install':
   provider => shell,
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name_;/server_name_;\n\trewrite ^\/redirect_me https:\/\/github.com\/MbaliFelecia permanent;/" /etc/nginx/site-available/default ; sudo service nginx start',
+}
